57d1c37cf57945a97498291845b5900c7f940e75
Richard Rosario <richardrosariojr@gmail.com>
2019-03-12 21:13:21 -0400

    adding the skeleton of the site

--- test_app.py
+++ test_app.py
@@ -6,29 +6,36 @@ init(token)
 
 
 # Leagues
-'''
 
+'''
 print('Leagues:')
 for l in leagues():
 	print(l['id'], l['name'])
 
-
+#gives us the league ID's 
+'''
+'''
 print('Squads:')
 for ts in team_squad():
 	print(ts['player_id'])
 
 '''
+
 '''
+
 print('Seasons:')
 for s in seasons():
 	print(s['id'], s['name'],s['league_id'])
+
+#list of seasons in both leauges
 '''
 
-'''
+
 print('Teams')
-for t in teams(12919):
+for t in teams(12963):
 	print(t['id'],t['name'])
-
+	
+'''
 
 print('Squads:')
 #for ts in team_squad(12919,1703):
@@ -37,7 +44,8 @@ for t in team_squad(12919,1703):
 	print(t['player_id'], player(t['player_id'])['lastname'])
 
 '''
-
+'''
 print('Player Data:')
 for ts in team_squad(12919,1703):
 	print (player(ts['player_id'])['lastname'], ts, '\n')
+'''
